334918f117a65dfb38dadc159b7b5d52dc132e70
Giovanni Monti <smartdavide@gmail.com>
2022-05-15 19:04:57 +0200

    bugfix

--- lua/autorun/gsquards_init.lua
+++ lua/autorun/gsquards_init.lua
@@ -4,22 +4,22 @@ gsquads = gsquads or {}
 if SERVER then
     include("g_squads/sv_factions.lua")
     include("g_squads/sv_squads.lua")
-    print("
+    print([[
     ---------------------------------\n
     ---------------------------------\n
                 GSquads 1.0          \n
     ------------ Loaded sv ----------\n
     ---------------------------------\n
-    ")
+    ]])
 end
 
 if CLIENT then
     include("g_squads/cl_squads.lua")
-    print("
+    print([[
     ---------------------------------\n
     ---------------------------------\n
                 GSquads 1.0          \n
     ------------ Loaded cl ----------\n
     ---------------------------------\n
-    ")
+    ]])
 end
