70b0cb590d28aa3df16ef56aa66c3bd6e1853da2
Giovanni Monti <smartdavide@gmail.com>
2022-05-20 19:58:01 +0200

    fixed loading issue

--- lua/autorun/gsquards_init.lua
+++ lua/autorun/gsquards_init.lua
@@ -1,45 +1,47 @@
 // this addon is for darkrp
-if not DarkRP then return end
+hook.Add('loadCustomDarkRPItems','gsquads_factions::postCustomDrp', function()
 
-gsquads = gsquads or {}
+    gsquads = gsquads or {}
 
-local function loadExtraFunctions()
+    local function loadExtraFunctions()
 
-    function gsquads.IsPlyNick( nick )
-        for _, v in pairs( player.GetAll() ) do
-            
-            if string.find( string.lower( v:Nick() ), string.lower( nick ), 1, true ) then return v end
-            if (v:SteamID64() == nick) or (v:SteamID() == nick) then return v end
-            
+        function gsquads.IsPlyNick( nick )
+            for _, v in pairs( player.GetAll() ) do
+                
+                if string.find( string.lower( v:Nick() ), string.lower( nick ), 1, true ) then return v end
+                if (v:SteamID64() == nick) or (v:SteamID() == nick) then return v end
+                
+            end
+            return false
         end
-        return false
+
+    end
+    if SERVER then
+        loadExtraFunctions()
+
+        include("g_squads/sv_factions.lua")
+        include("g_squads/sv_squads.lua")
+        include("g_squads/sv_chatcommands.lua")
+
+        --AddCSLuaFile("g_squads/cl_squads.lua")
+        print([[
+        ---------------------------------\n
+        ---------------------------------\n
+                    GSquads 1.0          \n
+        ------------ Loaded sv ----------\n
+        ---------------------------------\n
+        ]])
     end
 
-end
-if SERVER then
-    loadExtraFunctions()
+    if CLIENT then
+        --include("g_squads/cl_squads.lua")
+        print([[
+        ---------------------------------\n
+        ---------------------------------\n
+                    GSquads 1.0          \n
+        ------------ Loaded cl ----------\n
+        ---------------------------------\n
+        ]])
+    end
 
-    include("g_squads/sv_factions.lua")
-    include("g_squads/sv_squads.lua")
-    include("g_squads/sv_chatcommands.lua")
-
-    --AddCSLuaFile("g_squads/cl_squads.lua")
-    print([[
-    ---------------------------------\n
-    ---------------------------------\n
-                GSquads 1.0          \n
-    ------------ Loaded sv ----------\n
-    ---------------------------------\n
-    ]])
-end
-
-if CLIENT then
-    --include("g_squads/cl_squads.lua")
-    print([[
-    ---------------------------------\n
-    ---------------------------------\n
-                GSquads 1.0          \n
-    ------------ Loaded cl ----------\n
-    ---------------------------------\n
-    ]])
-end
+end)
